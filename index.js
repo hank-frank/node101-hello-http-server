f7310ad47aa5e293546c26010f1e909a990b7a84
hankfrank <henryfrank1@yahoo.com>
2019-06-17 18:29:32 -0700

    Initial commit of all files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code here
+const http = require ('http'); 
+
+const server = http.createServer(function (request, response){
+    console.log("recieved" + request.method + "request for" + request.url);
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    response.end("Hello World");
+});
+
+server.listen(8080, 'localhost', null, function(){
+    console.log("server is listening on localhost:8080")
+});
